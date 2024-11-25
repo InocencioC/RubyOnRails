bd3e8f258622196165bea9ba2e00a8df000d5bd7
iarmando <inocencio.armando@bancobai.ao>
2024-11-25 17:57:03 +0100

    feat: collections

---
+++ fundamentals/calculator.rb
@@ -0,0 +1,13 @@
+result = ''
+
+loop do 
+  puts result
+  puts 'Digite um número'
+  first_number = gets.chomp.to_i
+  puts 'Digite o segundo número'
+  second_number = gets.chomp.to_i
+
+
+  option = gets.chomp.to_i
+
+  if 
---
+++ fundamentals/each_array.rb
@@ -0,0 +1,9 @@
+names = ['Joãozinho', 'Manuel', 'Juca']
+
+name = 'Inocêncio Cordeiro'
+
+names.each do |name|
+  puts name + 'é o meu nome'
+end
+
+puts name
