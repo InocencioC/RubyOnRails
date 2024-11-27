a9e239b5c997b80b4566f3aeb8267c64543c57d3
iarmando <inocencio.armando@bancobai.ao>
2024-11-27 13:13:55 +0100

    feat: methods

---
+++ fundamentals/method.rb
@@ -0,0 +1,7 @@
+def talk(first_name, last_name)
+  puts "Olá, #{first_name} #{last_name} como você está ?"
+end
+
+first_name = 'Ino'
+last_name = 'Armando'
+talk(first_name, last_name)
---
+++ fundamentals/return.rb
@@ -0,0 +1,10 @@
+def retorno(a, b)
+  a > b
+end
+
+a = 2
+b = 1
+
+result = compare(a, b)
+
+puts "O resultado da comaparação '#{result}'"
