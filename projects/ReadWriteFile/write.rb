456ad4a11e625716a17969393ff92849b26015f1
iarmando <inocencio.armando@bancobai.ao>
2024-12-05 09:45:25 +0100

    feat: write and read files .txt

---
+++ projects/ReadWriteFile/write.rb
@@ -0,0 +1,17 @@
+File.open('shopping-list.txt', 'a') do |line|
+  line.puts('Farinha de trigo')
+  line.puts('Farinha de mandioca')
+  line.puts('Leite')
+  line.puts('Manteiga')
+  line.puts('Chá')
+  line.puts('Carnes')
+  line.puts('Peixes')
+  line.puts('Ovos')
+  end
+  
+  
+  
+  
+  
+  
+  
